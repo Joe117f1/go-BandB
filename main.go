b0b533ce7cdeaf2f92780a869837e8fb59779828
Yoav Hirshberg <yoav.yh@gmail.com>
2023-10-05 14:45:11 +0300

    update main file

--- main.go
+++ main.go
@@ -1,25 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-// User struct represents a user entity.
-type User struct {
-	ID       string `json:"id"`
-	UserName string `json:"userName"`
-	Email    string `json:"email"`
-}
-
 func main() {
 	router := mux.NewRouter()
 
 	// Define routes for CRUD operations on users.
-	router.HandleFunc("/users", CreateUser).Methods("POST")
+	router.HandleFunc("/users", CreateUserHandler).Methods("POST")
 	router.HandleFunc("/users", GetUsers).Methods("GET")
 	router.HandleFunc("/users/{id}", GetUser).Methods("GET")
 	router.HandleFunc("/users/{id}", UpdateUser).Methods("PUT")
@@ -29,22 +21,4 @@ func main() {
 	http.Handle("/", router)
 	fmt.Println("Server started on :8080")
 	http.ListenAndServe(":8080", nil)
-}
-
-// CreateUser handles the creation of a new user
-func CreateUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("CreateUser run")
-	var newUser User
-
-	err := json.NewDecoder(r.Body).Decode(&newUser)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	insertUser(newUser)
-	fmt.Println("users: ", users)
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(newUser)
-	w.WriteHeader(http.StatusCreated)
 }
