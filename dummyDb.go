c738089935d7ab1e0515da34dc9eaf95418ffbba
Yoav Hirshberg <yoav.yh@gmail.com>
2023-10-05 17:13:32 +0300

    create findUserById in dummyDb

--- dummyDb.go
+++ dummyDb.go
@@ -1,9 +1,31 @@
 package main
 
+import "errors"
+
 var users []User
 
 func insertUser(user User) error {
 	users = append(users, user)
 	return nil
 	// insert to db, might fail in reality
+}
+
+func findUserById(id string) (*User, error) {
+	var searchedUser *User
+	var err error
+
+	for _, user := range users {
+		if user.ID == id {
+			searchedUser = &user
+			err = nil
+			break
+		}
+	}
+
+	if searchedUser != nil {
+		return searchedUser, nil
+	}
+
+	err = errors.New("couldn'd find user")
+	return nil, err
 }
