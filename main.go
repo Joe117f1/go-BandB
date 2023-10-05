bf37767f853ea5f4426537221df97847c780a40a
Yoav Hirshberg <yoav.yh@gmail.com>
2023-10-05 19:16:50 +0300

    update main func to use dynamic strings

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
+const PORT_NUMBER = ":8080"
+
 func main() {
 	router := mux.NewRouter()
 
@@ -14,11 +16,11 @@ func main() {
 	router.HandleFunc("/users", CreateUser).Methods("POST")
 	router.HandleFunc("/users", GetUsers).Methods("GET")
 	router.HandleFunc(fmt.Sprintf("/users/{%s}", id), GetUserById).Methods("GET")
-	router.HandleFunc("/users/{id}", UpdateUser).Methods("PUT")
-	router.HandleFunc("/users/{id}", DeleteUser).Methods("DELETE")
+	router.HandleFunc(fmt.Sprintf("/users/{%s}", id), UpdateUser).Methods("PUT")
+	router.HandleFunc(fmt.Sprintf("/users/{%s}", id), DeleteUser).Methods("DELETE")
 
 	// Start the HTTP server.
 	http.Handle("/", router)
 	fmt.Println("Server started on :8080")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(PORT_NUMBER, nil)
 }
