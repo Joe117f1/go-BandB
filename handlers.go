5bb4678283b3861aa6bd5b98d351a2b80d45e4b2
Yoav Hirshberg <yoav.yh@gmail.com>
2023-10-05 14:44:35 +0300

    move other handlers (skeletons) into handlers file

--- handlers.go
+++ handlers.go
@@ -27,3 +27,23 @@ func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("users: ", users)
 	writeJsonResponse(w, newUser, http.StatusCreated)
 }
+
+// GetUsers returns a list of all users.
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	// Get users logic
+}
+
+// GetUser returns a specific user by ID.
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	// Get user by ID logic
+}
+
+// UpdateUser handles updating an existing user.
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	// Update user logic
+}
+
+// DeleteUser handles the deletion of a user by ID.
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	// Delete user by ID logic
+}
