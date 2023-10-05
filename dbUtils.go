f91ef6478cdc0d848ffeafc1a9bb4122a303df83
Yoav Hirshberg <yoav.yh@gmail.com>
2023-10-05 18:57:36 +0300

    add fetchUsers and add comments in dbUtils

--- dbUtils.go
+++ dbUtils.go
@@ -2,13 +2,17 @@ package main
 
 import "errors"
 
+// insertUser insert one ruser record to the db
 func insertUser(user User) error {
+	// insert to db logic...
 	users = append(users, user)
 	return nil
 	// insert to db, might fail in reality
 }
 
+// findUserById retrieve a user by id from db, return error if not found
 func findUserById(id string) (*User, error) {
+	// db query logic...
 	var searchedUser *User
 	var err error
 
@@ -26,4 +30,10 @@ func findUserById(id string) (*User, error) {
 
 	err = errors.New("couldn'd find user")
 	return nil, err
+}
+
+// fetchUsers fetch all users from the db
+func fetchUsers() (*Users, error) {
+	// db query logic...
+	return &users, nil
 }
